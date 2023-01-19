71fc4341660c6a8b5b5f2f8c9e86abc7cbc35f58
Rickelle <rickelle.1993@live.com.au>
2023-01-20 05:02:50 +1030

    Most of userControllers except add/delete friend

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -11,22 +11,36 @@ const {
 
 // /api/users
 router.route('/')
-  .get(getUsers)
-  .post(createUser);
+  .get(getUsers)   // GET http://localhost:3001/api/users WORKING
+  .post(createUser);   // POST http://localhost:3001/api/users WORKING
+
+  // {
+  //   "username": "Mary",
+  //   "email": "Mary@gmail.com",
+  //   "thoughts": [],
+  //   "friends": []
+  // }
 
 // /api/users/:usersId
 router.route('/:id')
-  .get(getSingleUser)
-  .delete(deleteUser)
-  .put(updateUser);
+  .get(getSingleUser)   // GET http://localhost:3001/api/users/:userid WORKING
+  .delete(deleteUser)   // DELETE http://localhost:3001/api/users/:userId WORKING
+  .put(updateUser);   // PUT http://localhost:3001/api/users/:userId WORKING
+  // {
+  //   "username": "Mary3",
+  //   "email": "Mary@gmail.com",
+  //   "thoughts": [],
+  //   "friends": []
+  // }
 
 // /api/users/:usersId/friends
 router.route('/:id/friends')
-  .post(addFriend);
+  .post(addFriend);   // POST http://localhost:3001/api/users/:userId/friends WORKING
 
 // /api/users/:usersId/friendss/:friendId
-router.route('/:id/friends/:friendsId')
-  .delete(removeFriend);
+router.route('/:id/friends/:friendsId') 
+  .delete(removeFriend); // DELETE http://localhost:3001/api/users/:usersId/friends/:friendsId
+
 
 module.exports = router;
 
